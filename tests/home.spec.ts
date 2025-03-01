bd1f4e708abfa4b1c4210933dcdb3c28e2fb57d5
RK <r.kaliakin@gmail.com>
2025-03-01 11:08:55 -0500

    adjust formatting and remove comments in homepage test
    Signed-off-by: RK <r.kaliakin@gmail.com>

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,9 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('Homepage should display the welcome heading', async ({page}) => {
-    // Given I navigate to the homepage
+test('Homepage should display the welcome heading', async ({ page }) => {
     await page.goto('/');
 
-    // Then welcome heading should be visible
     await expect(page.getByRole('heading', { name: 'Welcome to the-internet' })).toBeVisible();
-})
+});
