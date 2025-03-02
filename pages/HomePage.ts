d9da81789f1571a1f1ca0b46a90271fa5057c778
RK <r.kaliakin@gmail.com>
2025-03-02 09:47:27 -0500

    Add `examplesHeading` locator in `HomePage.ts`
    Signed-off-by: RK <r.kaliakin@gmail.com>

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -3,11 +3,15 @@ import { Locator, Page } from '@playwright/test';
 export class HomePage {
   readonly page: Page;
   readonly welcomeHeading: Locator;
+  readonly examplesHeading: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.welcomeHeading = page.getByRole('heading', {
       name: 'Welcome to the-internet',
+    });
+    this.examplesHeading = page.getByRole('heading', {
+      name: 'Available Examples',
     });
   }
 
