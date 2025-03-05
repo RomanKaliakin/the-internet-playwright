7ced3117a0f61b6315bab8e5a47ed5c552b4f4f8
RK <r.kaliakin@gmail.com>
2025-03-05 10:58:35 -0500

    Add `addElementButton` locator in `AddRemoveElementsPage.ts`

--- pages/AddRemoveElementsPage.ts
+++ pages/AddRemoveElementsPage.ts
@@ -3,9 +3,11 @@ import { Locator, Page } from '@playwright/test';
 export class AddRemoveElementsPage {
   readonly page: Page;
   readonly heading: Locator;
+  readonly addElementButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.heading = page.getByRole('heading', { name: 'Add/Remove Elements' });
+    this.addElementButton = page.getByRole('button', { name: 'Add Element' });
   }
 }
