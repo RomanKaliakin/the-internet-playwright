82f1d6490e4fbf306d628178ac4e0432da99afb3
RK <r.kaliakin@gmail.com>
2025-03-05 11:32:55 -0500

    Add `deleteButtons` locator in `AddRemoveElementsPage.ts`
    Signed-off-by: RK <r.kaliakin@gmail.com>

--- pages/AddRemoveElementsPage.ts
+++ pages/AddRemoveElementsPage.ts
@@ -4,10 +4,12 @@ export class AddRemoveElementsPage {
   readonly page: Page;
   readonly heading: Locator;
   readonly addElementButton: Locator;
+  readonly deleteButtons: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.heading = page.getByRole('heading', { name: 'Add/Remove Elements' });
     this.addElementButton = page.getByRole('button', { name: 'Add Element' });
+    this.deleteButtons = page.getByRole('button', { name: 'Delete' });
   }
 }
