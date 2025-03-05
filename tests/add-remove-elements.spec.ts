cb338fdf1a3dc4a8ac1dca0bf7c16341155c8fab
RK <r.kaliakin@gmail.com>
2025-03-05 11:40:27 -0500

    Wrap tests related to initial state inside `Initial State` describe block in `add-remove-elements.spec.ts`
    Signed-off-by: RK <r.kaliakin@gmail.com>

--- tests/add-remove-elements.spec.ts
+++ tests/add-remove-elements.spec.ts
@@ -6,21 +6,25 @@ test.describe('Add/Remove Elements page', () => {
     await page.goto('/add_remove_elements/');
   });
 
-  test('should display the Add/Remove Elements heading', async ({ page }) => {
-    const addRemovePage = new AddRemoveElementsPage(page);
-    await expect(addRemovePage.heading).toBeVisible();
-  });
+  test.describe('Initial State', () => {
+    test('should display the Add/Remove Elements heading', async ({ page }) => {
+      const addRemovePage = new AddRemoveElementsPage(page);
+      await expect(addRemovePage.heading).toBeVisible();
+    });
 
-  test('should display the "Add Element" button by default', async ({
-    page,
-  }) => {
-    const addRemovePage = new AddRemoveElementsPage(page);
-    await expect(addRemovePage.addElementButton).toBeVisible();
-    await expect(addRemovePage.addElementButton).toBeEnabled();
-  });
+    test('should display the "Add Element" button by default', async ({
+      page,
+    }) => {
+      const addRemovePage = new AddRemoveElementsPage(page);
+      await expect(addRemovePage.addElementButton).toBeVisible();
+      await expect(addRemovePage.addElementButton).toBeEnabled();
+    });
 
-  test('should not display any Delete buttons by default', async ({ page }) => {
-    const addRemoveElementsPage = new AddRemoveElementsPage(page);
-    await expect(addRemoveElementsPage.deleteButtons).toHaveCount(0);
+    test('should not display any Delete buttons by default', async ({
+      page,
+    }) => {
+      const addRemoveElementsPage = new AddRemoveElementsPage(page);
+      await expect(addRemoveElementsPage.deleteButtons).toHaveCount(0);
+    });
   });
 });
